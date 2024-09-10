c5b249cdf5b7f5b29980582a1b93d1f87d126f90
sumit kumar <sumitkumar@sumits-MacBook-Air.local>
2024-09-11 00:01:36 +0530

    nn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,19 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/bs', 'react-icons/bi']
-    }
-  }
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-icons/bs', 'react-icons/bi']
+//     }
+//   }
+// })
